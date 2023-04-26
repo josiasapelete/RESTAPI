4006dda74456a877a21179bd9ad950ca651871fc
josiasapelete <josiasapelete@gmail.com>
2023-04-26 10:24:12 +0000

    port

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+app.set('port', PORT);
+app.set('env', NODE_ENV);
+
+
 const PostRoute =require("./routes/post.routes")
 require("dotenv").config()
 const connectDB = require("./config/db")
@@ -16,6 +22,10 @@ app.use(express.urlencoded({extended:false}));
 
 app.use("/",PostRoute);
 
-app.listen(PORT,()=>{
-    console.log("Attende de requete au port "+ PORT)
-})
+app.listen(PORT, () => {
+    console.log(
+      `Express Server started on Port ${app.get(
+        'port'
+      )} | Environment : ${app.get('env')}`
+    );
+  });
