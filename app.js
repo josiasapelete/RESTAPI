ff0c8e8a82c8cfdd85f99f8af9fb5e4ba638c25f
josiasapelete <josiasapelete@gmail.com>
2023-04-26 10:20:02 +0000

    port

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const PostRoute =require("./routes/post.routes")
 require("dotenv").config()
 const connectDB = require("./config/db")
